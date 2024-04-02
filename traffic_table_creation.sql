5f2f06a3401f7117d15fdf8d582e18101f910c59
Noelle Matthews <146158794+noellematthews@users.noreply.github.com>
2024-04-02 14:52:02 -0700

    Create traffic_table_creation.sql

---
+++ traffic_table_creation.sql
@@ -0,0 +1,22 @@
+-- Create table for storing traffic incident data
+CREATE TABLE traffic_incidents (
+    incident_id SERIAL PRIMARY KEY,
+    start_location VARCHAR(255),
+    end_location VARCHAR(255),
+    road_name VARCHAR(255),
+    type_of_delay VARCHAR(255),
+    delay_length INT,
+    significance VARCHAR(255),
+    distance INT,
+    incident_time TIMESTAMP
+);
+
+-- Create table for storing traffic flow data
+CREATE TABLE traffic_flow (
+    flow_id SERIAL PRIMARY KEY,
+    segment_id INT,
+    current_speed INT,
+    freeflow_speed INT,
+    quality_indicator INT,
+    flow_time TIMESTAMP
+);
